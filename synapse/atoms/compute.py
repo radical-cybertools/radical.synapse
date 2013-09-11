990107da16c0ca0d8e10a578595fd27140559aae
Andre Merzky <andre@merzky.net>
2013-09-11 12:32:37 +0200

    add memory load

--- synapse/atoms/compute.py
+++ synapse/atoms/compute.py
@@ -8,12 +8,13 @@ FLOPS  = "Flops"
 
 
 import os
+import textwrap
 import subprocess
 
 import saga.utils.signatures   as sus
 
-from base      import AtomBase
-from constants import COMPUTE
+from   base      import AtomBase
+from   constants import COMPUTE
 
 # ------------------------------------------------------------------------------
 #
@@ -33,40 +34,51 @@ class Compute (AtomBase) :
 
         AtomBase.__init__ (self, COMPUTE, info)
 
+        # workload property for this atom
+        self.load_flops  = 1000
+        self.load_malloc = 1000
+
         # create our C-based workload script in tmp space
-        
-        flops_file = "%s/flops" % self._tmpdir
+        self._flops_exe  = "%s/synapse_flops"  % self._tmpdir
+        self._malloc_exe = "%s/synapse_malloc" % self._tmpdir
 
-        if  os.path.isfile (flops_file) :
-            # already have the flops tool
-            return
+        # already have the flops tool?
+        if  not os.path.isfile (self._flops_exe) :
 
-        # if not, we compile it on the fly...
-        flops = """
+            # if not, we compile it on the fly...
+            # Note that the program below will actually, for each flop, also create
+            # 3 INTEGER OPs and 1 Branching instruction.
+            flops_code = open (os.path.dirname(__file__) + '/synapse_flops.c').read ()
 
-#include <stdlib.h>
+            p = subprocess.Popen ("cc -x c -O0 -o %s -" % self._flops_exe,
+                                  shell=True,
+                                  stdin=subprocess.PIPE, 
+                                  stdout=subprocess.PIPE, 
+                                  stderr=subprocess.PIPE)
+            (pout, perr) = p.communicate (textwrap.dedent (flops_code))
 
-int main()
-{
-  long int i, I = atol (getenv ("ITER")) * 1000000;
-  double   f = 1.0;
+            if  p.returncode :
+                raise Exception("Couldn't create flops: %s : %s" % (pout, perr))
 
-  for (i = 0; i < I; i++ )
-  {
-    f = f * 1.000000000001;
-  }
-}
 
-"""
 
-        p = subprocess.Popen ("cc -x c -O0 -o %s/flops -" % self._tmpdir, 
-                              shell=True,
-                              stdin=subprocess.PIPE, 
-                              stdout=subprocess.PIPE, 
-                              stderr=subprocess.PIPE)
-        (pout, perr) = p.communicate (flops)
-        if  pout: print pout
-        if  perr: print perr
+        # already have the malloc tool?
+        if  not os.path.isfile (self._malloc_exe) :
+
+            # if not, we compile it on the fly...
+            # Note that the program below will actually, for each flop, also create
+            # 3 INTEGER OPs and 1 Branching instruction.
+            malloc_code = open (os.path.dirname(__file__) + '/synapse_malloc.c').read ()
+
+            p = subprocess.Popen ("cc -x c -O0 -o %s -" % self._malloc_exe,
+                                  shell=True,
+                                  stdin=subprocess.PIPE, 
+                                  stdout=subprocess.PIPE, 
+                                  stderr=subprocess.PIPE)
+            (pout, perr) = p.communicate (textwrap.dedent (malloc_code))
+
+            if  p.returncode :
+                raise Exception("Couldn't create malloc: %s : %s" % (pout, perr))
 
 
 
@@ -76,8 +88,11 @@ int main()
     @sus.returns (sus.nothing)
     def work (self) :
 
-        print "start work"
-        p = subprocess.Popen ("ITER=1000 %s/flops -" % self._tmpdir, 
+
+        # allocate requested amount of memory, and run requested number of flops
+
+        print "start malloc"
+        p = subprocess.Popen ("%s %d" % (self._malloc_exe, self.load_malloc), 
                               shell=True,
                               stdin=subprocess.PIPE, 
                               stdout=subprocess.PIPE, 
@@ -85,7 +100,18 @@ int main()
         (pout, perr) = p.communicate ()
         if  pout: print pout
         if  perr: print perr
-        print "stop  work"
+        print "stop  malloc"
+
+        print "start flops"
+        p = subprocess.Popen ("%s %d" % (self._flops_exe, self.load_flops), 
+                              shell=True,
+                              stdin=subprocess.PIPE, 
+                              stdout=subprocess.PIPE, 
+                              stderr=subprocess.PIPE)
+        (pout, perr) = p.communicate ()
+        if  pout: print pout
+        if  perr: print perr
+        print "stop  flops"
 
 # vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
