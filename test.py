d458ba3a121fb39ba2380bea5638413407d4a3f9
Andre Merzky <andre@merzky.net>
2013-10-07 13:24:00 -0400

    comments

--- test.py
+++ test.py
@@ -1,26 +1,36 @@
 
 import synapse.atoms as sa
+import time
 
+# create containers for different system workload types
 sac = sa.Compute ()
 sam = sa.Memory  ()
 sas = sa.Storage ()
 san = sa.Network ()
 
-sac.run (info={'n' : 100})
-sam.run (info={'n' : 100})
-sas.run (info={'n' : 100, 'tgt' : '%(tmp)s/synapse_storage.tmp.%(pid)s'})
-san.run (info={'type'   : 'server',
+# the atoms below are executed concurrently (in their own threads)
+sac.run (info={'n' : 1000}) # consume  1 GFlop CPY Cycles
+sam.run (info={'n' : 1000}) # allocate 1 GByte memory
+sas.run (info={'n' : 1000,  # write    1 GByte to disk
+               'tgt' : '%(tmp)s/synapse_storage.tmp.%(pid)s'})
+
+san.run (info={'type'   : 'server', # communicate a 1 GByte message
                'mode'   : 'read',
                'port'   : 10000,
-               'n'      : 1})
+               'n'      : 1000})
+time.sleep (1)
 san.run (info={'type'   : 'client',
                'mode'   : 'write',
                'host'   : 'localhost',
                'port'   : 10000,
-               'n'      : 1})
+               'n'      : 1000})
 
+# wait 'til all atoms are done
 sac.wait ()
 sam.wait ()
 sas.wait ()
 san.wait ()
+
+# burn some more cyles, for the fun of it
+sac.run (info={'n' : 1000}) # consume  1 GFlop CPY Cycles
 
