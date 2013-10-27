102a5621a3b773c2a814a80231f534473076e31f
Andre Merzky <andre@merzky.net>
2013-10-27 10:13:21 +0100

    move dat

--- synapse/atoms/synapse_memory.c
+++ synapse/atoms/synapse_memory.c
@@ -5,24 +5,23 @@
 
 int main (int argc, char** argv)
 {
-  /* size */
-  if ( argc < 2 )
-  {
-    return -1;
-  }
-
-  size_t n = atol   (argv[1]) * CHUNKSIZE;
-  char*  c = malloc (n * sizeof(char));
-
-  /* make sure the memory actually got allocated */
-  unsigned long int i = 0;
-  for ( i = 0; i < n; i++ ) {
-    c[i] = '\0';
-  }
-
-  free (c);
-
-  return 0;
-
+    /* size */
+    if ( argc < 2 )
+    {
+        return -1;
+    }
+  
+    size_t n   = atol   (argv[1]) * CHUNKSIZE;
+    char*  c = malloc (n * sizeof(char));
+  
+    /* make sure the memory actually got allocated */
+    unsigned long int i = 0;
+    for ( i = 0; i < n; i++ ) {
+        c[i] = '\0';
+    }
+  
+    free (c);
+  
+    return 0;
 }
 
