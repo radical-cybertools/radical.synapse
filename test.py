177222d1899a553d03a962e34ef7b0f0ac8a88ba
Andre Merzky <andre@merzky.net>
2013-11-10 14:23:34 -0500

    up

--- test.py
+++ test.py
@@ -106,7 +106,8 @@ with open ('%s/synapse/experiments/%s.dat' % (home, host), 'a') as f :
 #   print output
     f.write ("%s\n" % output)
 
-#   time.sleep (10)
+    _ = os.popen ('ps -ef | grep -i "/tmp/synapse/" | grep -v grep | cut -c 8-15 | xargs -r kill -9')
+    time.sleep (10)
 
 #   print su.get_mem_usage ()
 #   print su.get_io_usage  ()
