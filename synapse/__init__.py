df434bed922eddde4deeff3d4c3ec1951a31ff97
Andre Merzky <andre@merzky.net>
2013-12-02 00:12:48 +0100

    add missing files, version

---
+++ synapse/__init__.py
@@ -0,0 +1,12 @@
+
+__author__    = "Radical.Utils Development Team (Andre Merzky, Ole Weidner)"
+__copyright__ = "Copyright 2013, RADICAL@Rutgers"
+__license__   = "MIT"
+
+
+import os
+
+version=open (os.path.dirname (os.path.abspath (__file__)) + "/VERSION", 'r').read().strip()
+
+
+
