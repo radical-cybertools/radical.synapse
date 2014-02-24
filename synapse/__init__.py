ba7f8dd2b7f71274e0ccb29da86ceff452f3885b
Andre Merzky <andre@merzky.net>
2014-02-24 13:25:01 +0100

    version fix

--- synapse/__init__.py
+++ synapse/__init__.py
@@ -5,6 +5,7 @@ __license__   = "MIT"
 
 
 import os
+import radical.utils        as ru
 import radical.utils.logger as rul
 
 
@@ -23,9 +24,17 @@ from utils     import emulate_command
 
 # ------------------------------------------------------------------------------
 
-version = open (os.path.dirname (os.path.abspath (__file__)) + "/VERSION", 'r').read().strip()
+
+pwd     = os.path.dirname (__file__)
+root    = "%s/.." % pwd
+short_version, long_version, branch = ru.get_version ([root, pwd])
+version = long_version
+
+# FIXME: the logger init will require a 'classical' ini based config, which is
+# different from the json based config we use now.   May need updating once the
+# radical configuration system has changed to json
 _logger = rul.logger.getLogger  ('synapse')
-_logger.info                    ('synapse         version: %s' % version)
+_logger.info ('synapse         version: %s' % version)
 
 
 # ------------------------------------------------------------------------------
