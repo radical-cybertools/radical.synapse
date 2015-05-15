501d96255b93ef38b41f6b2b8dd06e5709238ae9
Andre Merzky <andre@merzky.net>
2015-05-15 15:22:17 +0200

    snapshot

--- src/radical/synapse/atoms/synapse_compute_asm.c
+++ src/radical/synapse/atoms/synapse_compute_asm.c
@@ -11,7 +11,9 @@
 #include <sys/types.h>
 #include <sys/resource.h>
 
-#define CHUNKSIZE  (1024 * 256)  /* 10^6 */
+/* NOTE: tune this number to tune loop towards 1 MFlop */
+#define CHUNKSIZE  (1024 * 1024)  /* 2^20 */
+#define PROFILE     0
 
 float mat_mult (float* a1, float* a2, int n);
 
@@ -23,46 +25,55 @@ int main (int argc, char** argv)
         return -1;
     }
 
-    struct rusage ru;
     size_t n = atol (argv[1]);
     float f[CHUNKSIZE];
 
+    /* This is an overhead for each sample :( */
     unsigned int i = 0;
     for ( i = 0; i < CHUNKSIZE; i++ ) 
     { 
         f[i] = i * 0.01;
     }
 
-    for ( i = 0; i < n * 13; i++ ) 
+    /* *********************************
+     * 1 loop gives 1 MFLOP
+     */
+    n = n / (1024 * 1024)
+    for ( i = 0; i < n; i++ ) 
     {
         mat_mult (f, f, CHUNKSIZE);
     }
+    /* ********************************* */
 
-
-    if ( 0 != getrusage (RUSAGE_SELF, &ru) )
+    if (PROFILE)
     {
-        fprintf (stderr, "no ru: %s", strerror (errno));
-        return  (1);
-    }
+        struct rusage ru;
 
-    fprintf (stdout, "ru.utime         : %ld.%ld\n", ru.ru_utime.tv_sec,
-                                                     ru.ru_utime.tv_usec ); /* user CPU time used */
-    fprintf (stdout, "ru.stime         : %ld.%ld\n", ru.ru_stime,
-                                                     ru.ru_stime.tv_usec ); /* system CPU time used */
-    fprintf (stdout, "ru.maxrss        : %ld\n",     ru.ru_maxrss*1024   ); /* maximum resident set size */
-    fprintf (stdout, "ru.ixrss         : %ld\n",     ru.ru_ixrss         ); /* integral shared memory size */
-    fprintf (stdout, "ru.idrss         : %ld\n",     ru.ru_idrss         ); /* integral unshared data size */
-    fprintf (stdout, "ru.isrss         : %ld\n",     ru.ru_isrss         ); /* integral unshared stack size */
-    fprintf (stdout, "ru.minflt        : %ld\n",     ru.ru_minflt        ); /* page reclaims (soft page faults) */
-    fprintf (stdout, "ru.majflt        : %ld\n",     ru.ru_majflt        ); /* page faults (hard page faults) */
-    fprintf (stdout, "ru.nswap         : %ld\n",     ru.ru_nswap         ); /* swaps */
-    fprintf (stdout, "ru.inblock       : %ld\n",     ru.ru_inblock       ); /* block input operations */
-    fprintf (stdout, "ru.outblock      : %ld\n",     ru.ru_oublock       ); /* block output operations */
-    fprintf (stdout, "ru.msgsnd        : %ld\n",     ru.ru_msgsnd        ); /* IPC messages sent */
-    fprintf (stdout, "ru.msgrcv        : %ld\n",     ru.ru_msgrcv        ); /* IPC messages received */
-    fprintf (stdout, "ru.nsignals      : %ld\n",     ru.ru_nsignals      ); /* signals received */
-    fprintf (stdout, "ru.nvcsw         : %ld\n",     ru.ru_nvcsw         ); /* voluntary context switches */
-    fprintf (stdout, "ru.nivcsw        : %ld\n",     ru.ru_nivcsw        ); /* involuntary context switches */
+        if ( 0 != getrusage (RUSAGE_SELF, &ru) )
+        {
+            fprintf (stderr, "no ru: %s", strerror (errno));
+            return  (1);
+        }
+
+        fprintf (stdout, "ru.utime         : %ld.%ld\n", ru.ru_utime.tv_sec,
+                                                         ru.ru_utime.tv_usec ); /* user CPU time used */
+        fprintf (stdout, "ru.stime         : %ld.%ld\n", ru.ru_stime,
+                                                         ru.ru_stime.tv_usec ); /* system CPU time used */
+        fprintf (stdout, "ru.maxrss        : %ld\n",     ru.ru_maxrss*1024   ); /* maximum resident set size */
+        fprintf (stdout, "ru.ixrss         : %ld\n",     ru.ru_ixrss         ); /* integral shared memory size */
+        fprintf (stdout, "ru.idrss         : %ld\n",     ru.ru_idrss         ); /* integral unshared data size */
+        fprintf (stdout, "ru.isrss         : %ld\n",     ru.ru_isrss         ); /* integral unshared stack size */
+        fprintf (stdout, "ru.minflt        : %ld\n",     ru.ru_minflt        ); /* page reclaims (soft page faults) */
+        fprintf (stdout, "ru.majflt        : %ld\n",     ru.ru_majflt        ); /* page faults (hard page faults) */
+        fprintf (stdout, "ru.nswap         : %ld\n",     ru.ru_nswap         ); /* swaps */
+        fprintf (stdout, "ru.inblock       : %ld\n",     ru.ru_inblock       ); /* block input operations */
+        fprintf (stdout, "ru.outblock      : %ld\n",     ru.ru_oublock       ); /* block output operations */
+        fprintf (stdout, "ru.msgsnd        : %ld\n",     ru.ru_msgsnd        ); /* IPC messages sent */
+        fprintf (stdout, "ru.msgrcv        : %ld\n",     ru.ru_msgrcv        ); /* IPC messages received */
+        fprintf (stdout, "ru.nsignals      : %ld\n",     ru.ru_nsignals      ); /* signals received */
+        fprintf (stdout, "ru.nvcsw         : %ld\n",     ru.ru_nvcsw         ); /* voluntary context switches */
+        fprintf (stdout, "ru.nivcsw        : %ld\n",     ru.ru_nivcsw        ); /* involuntary context switches */
+    }
 
     return 0;
 }
--- src/radical/synapse/atoms/synapse_memory.c
+++ src/radical/synapse/atoms/synapse_memory.c
@@ -12,8 +12,8 @@
 #include <sys/resource.h>
 
 
-
 #define CHUNKSIZE  (1024 * 1024)  /* 2^20 */
+#define RUSAGE     0
 
 int main (int argc, char** argv)
 {
@@ -23,7 +23,11 @@ int main (int argc, char** argv)
         return -1;
     }
 
-    struct rusage ru;
+    /* *********************************
+     * 1 loop iter allocates 1 MByte
+     */
+    n = n / CHUNKSIZE
+
     size_t n = atol   (argv[1]) * CHUNKSIZE;
     char*  c = malloc (n * sizeof(char));
 
@@ -35,31 +39,36 @@ int main (int argc, char** argv)
     }
 
     free (c);
-  
-    if ( 0 != getrusage (RUSAGE_SELF, &ru) )
-    {
-        fprintf (stderr, "no ru: %s", strerror (errno));
-        return (1);
-    }
 
-    fprintf (stdout, "ru.Utime         : %ld.%ld\n", ru.ru_utime.tv_sec,
-                                                     ru.ru_utime.tv_usec ); /* user CPU time used */
-    fprintf (stdout, "ru.stime         : %ld.%ld\n", ru.ru_stime,
-                                                     ru.ru_stime.tv_usec ); /* system CPU time used */
-    fprintf (stdout, "ru.maxrss        : %ld\n",     ru.ru_maxrss*1024   ); /* maximum resident set size */
-    fprintf (stdout, "ru.ixrss         : %ld\n",     ru.ru_ixrss         ); /* integral shared memory size */
-    fprintf (stdout, "ru.idrss         : %ld\n",     ru.ru_idrss         ); /* integral unshared data size */
-    fprintf (stdout, "ru.isrss         : %ld\n",     ru.ru_isrss         ); /* integral unshared stack size */
-    fprintf (stdout, "ru.minflt        : %ld\n",     ru.ru_minflt        ); /* page reclaims (soft page faults) */
-    fprintf (stdout, "ru.majflt        : %ld\n",     ru.ru_majflt        ); /* page faults (hard page faults) */
-    fprintf (stdout, "ru.nswap         : %ld\n",     ru.ru_nswap         ); /* swaps */
-    fprintf (stdout, "ru.inblock       : %ld\n",     ru.ru_inblock       ); /* block input operations */
-    fprintf (stdout, "ru.outblock      : %ld\n",     ru.ru_oublock       ); /* block output operations */
-    fprintf (stdout, "ru.msgsnd        : %ld\n",     ru.ru_msgsnd        ); /* IPC messages sent */
-    fprintf (stdout, "ru.msgrcv        : %ld\n",     ru.ru_msgrcv        ); /* IPC messages received */
-    fprintf (stdout, "ru.nsignals      : %ld\n",     ru.ru_nsignals      ); /* signals received */
-    fprintf (stdout, "ru.nvcsw         : %ld\n",     ru.ru_nvcsw         ); /* voluntary context switches */
-    fprintf (stdout, "ru.nivcsw        : %ld\n",     ru.ru_nivcsw        ); /* involuntary context switches */
+    if ( PROFILE )
+    {
+        struct rusage ru;
+
+        if ( 0 != getrusage (RUSAGE_SELF, &ru) )
+        {
+            fprintf (stderr, "no ru: %s", strerror (errno));
+            return (1);
+        }
+
+        fprintf (stdout, "ru.Utime         : %ld.%ld\n", ru.ru_utime.tv_sec,
+                                                         ru.ru_utime.tv_usec ); /* user CPU time used */
+        fprintf (stdout, "ru.stime         : %ld.%ld\n", ru.ru_stime,
+                                                         ru.ru_stime.tv_usec ); /* system CPU time used */
+        fprintf (stdout, "ru.maxrss        : %ld\n",     ru.ru_maxrss*1024   ); /* maximum resident set size */
+        fprintf (stdout, "ru.ixrss         : %ld\n",     ru.ru_ixrss         ); /* integral shared memory size */
+        fprintf (stdout, "ru.idrss         : %ld\n",     ru.ru_idrss         ); /* integral unshared data size */
+        fprintf (stdout, "ru.isrss         : %ld\n",     ru.ru_isrss         ); /* integral unshared stack size */
+        fprintf (stdout, "ru.minflt        : %ld\n",     ru.ru_minflt        ); /* page reclaims (soft page faults) */
+        fprintf (stdout, "ru.majflt        : %ld\n",     ru.ru_majflt        ); /* page faults (hard page faults) */
+        fprintf (stdout, "ru.nswap         : %ld\n",     ru.ru_nswap         ); /* swaps */
+        fprintf (stdout, "ru.inblock       : %ld\n",     ru.ru_inblock       ); /* block input operations */
+        fprintf (stdout, "ru.outblock      : %ld\n",     ru.ru_oublock       ); /* block output operations */
+        fprintf (stdout, "ru.msgsnd        : %ld\n",     ru.ru_msgsnd        ); /* IPC messages sent */
+        fprintf (stdout, "ru.msgrcv        : %ld\n",     ru.ru_msgrcv        ); /* IPC messages received */
+        fprintf (stdout, "ru.nsignals      : %ld\n",     ru.ru_nsignals      ); /* signals received */
+        fprintf (stdout, "ru.nvcsw         : %ld\n",     ru.ru_nvcsw         ); /* voluntary context switches */
+        fprintf (stdout, "ru.nivcsw        : %ld\n",     ru.ru_nivcsw        ); /* involuntary context switches */
+    }
 
     return 0;
 }
