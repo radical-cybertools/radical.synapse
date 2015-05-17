12fc8f4921fa4f751de5801dbfb06f1ca2d49726
Andre Merzky <andre@merzky.net>
2015-05-18 00:09:36 +0200

    tag handling fixes

--- tests/test.py
+++ tests/test.py
@@ -10,18 +10,41 @@ import radical.synapse       as rs
 import radical.synapse.atoms as rsa
 
 os.environ['RADICAL_SYNAPSE_DBURL'] = "file://%s" % os.getcwd ()
-os.environ['RADICAL_SYNAPSE_TAGS']  = "sto:sto"
 print os.environ['RADICAL_SYNAPSE_DBURL']
 
+
+print "------------------------------------------------------------------------"
+os.environ['RADICAL_SYNAPSE_TAGS']  = "cpu"
 info, _, _ = rs.emulate ('test cpu')
 pprint.pprint (info)
 
+print "------------------------------------------------------------------------"
+os.environ['RADICAL_SYNAPSE_TAGS']  = "mem"
 info, _, _ = rs.emulate ('test mem')
 pprint.pprint (info)
 
+print "------------------------------------------------------------------------"
+os.environ['RADICAL_SYNAPSE_TAGS']  = "sto"
 info, _, _ = rs.emulate ('test sto')
 pprint.pprint (info)
 
+print "------------------------------------------------------------------------"
+os.environ['RADICAL_SYNAPSE_TAGS']  = ""
 info, _, _ = rs.emulate ('test')
 pprint.pprint (info)
+
+
+print "------------------------------------------------------------------------"
+info, _, _ = rs.execute ('sleep 3')
+pprint.pprint (info)
+
+print "------------------------------------------------------------------------"
+info, _, _ = rs.profile ('sleep 3')
+pprint.pprint (info)
+
+print "------------------------------------------------------------------------"
+info, _, _ = rs.emulate ('sleep 3')
+pprint.pprint (info)
+
+print "------------------------------------------------------------------------"
 
