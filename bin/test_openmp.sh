aaf330b9258ea34eb1fd6191aa556d59595c504e
Andre Merzky <andre@merzky.net>
2017-05-12 08:55:26 +0200

    add test script

---
+++ bin/test_openmp.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+VE=./ve
+VE=/lustre/atlas2/csc230/scratch/merzky1/radical.pilot.sandbox
+
+. $VE/bin/activate
+
+
+export OMP_NUM_THREADS=$1
+export RADICAL_SYNAPSE_USE_OPENMP=1
+
+start=$(date +%s)
+radical-synapse-sample -s 10 -f 10000000000
+stop=$(date +%s)
+
+echo "diff: $((stop-start))"
+ 
