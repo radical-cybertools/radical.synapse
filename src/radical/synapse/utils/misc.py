af98173fc7d72fd388500ec24a811aaa8507c395
Andre Merzky <andre@merzky.net>
2019-07-13 02:46:06 +0200

    Rc/v0.70.0 (#20)
    * version bump
    
    * setup sync
    
    * Committing Travis YML file

--- src/radical/synapse/utils/misc.py
+++ src/radical/synapse/utils/misc.py
@@ -25,7 +25,7 @@ else:
 
 # ------------------------------------------------------------------------------
 #
-logger = ru.get_logger('radical.synapse')
+logger = ru.Logger('radical.synapse')
 
 
 # ------------------------------------------------------------------------------
@@ -406,7 +406,7 @@ def make_frames (docs):
 
         frame_dicts.append(frame_dict)
 
-    import pandas 
+    import pandas
     frames = pandas.DataFrame (frame_dicts)
 
     return frames
