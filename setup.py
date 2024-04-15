65a8483ace4681ab3f3024e9a8ed73a25f992bc2
Andre Merzky <andre@merzky.net>
2024-04-15 08:15:30 +0200

    hotfix to get install working after setuptools update

--- setup.py
+++ setup.py
@@ -137,6 +137,10 @@ def get_version(_mod_root):
       # _sdist_name = _sdist_name.replace('#', '-')
       # _sdist_name = _sdist_name.replace('_', '-')
 
+        # setuptools 69.5 does changes naming scheme
+        if not os.path.isfile('%s/%s' % (_path, _sdist_name)):
+            _sdist_name = '%s-%s.tar.gz' % (name.replace('.', '_'), _version_base)
+
         if '--record'    in sys.argv or \
            'bdist_egg'   in sys.argv or \
            'bdist_wheel' in sys.argv    :
@@ -217,7 +221,7 @@ c_ext = Extension(name               = "radical/synapse/atoms/_atoms" ,
 #
 setup_args = {
     'name'               : name,
-    'namespace_packages' : ['radical'],
+  # 'namespace_packages' : ['radical'],
     'version'            : version,
     'description'        : 'SYNthetic APplicationS Emulator',
     'author'             : 'RADICAL Group at Rutgers University',
